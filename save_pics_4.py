29e4d3e144f44bc87253d782dfffa28ca88a1d38
Clamator <kalmah23@gmail.com>
2022-01-11 21:58:39 +0300

    1

---
+++ save_pics_4.py
@@ -0,0 +1,40 @@
+from multiprocessing import Pool
+
+import time
+
+import wget
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+
+def get_links():
+    option = Options()
+    option.add_argument("--disable-blink-features=AutomationControlled")
+    option.headless = True
+    driver = webdriver.Chrome(chrome_options=option)
+    start_page = 'https://nsfwalbum.com/photo/72226862'
+    driver.get(start_page)
+
+    all_links = []
+    for _ in range(90):
+        time.sleep(0.6)
+        link = driver.find_element(By.CSS_SELECTOR, 'img').get_attribute('src')
+        all_links.append(link)
+        print(link)
+        time.sleep(0.2)
+        next = driver.find_element(By.CSS_SELECTOR, 'a[id*="nextPhoto"]')
+        driver.execute_script("arguments[0].click();", next)
+
+    return all_links
+
+
+
+def wget_download(urls):
+    directory = 'F:\Test2'
+    for url in urls:
+        wget.download(url, out=directory)
+
+
+if __name__ == '__main__':
+    wget_download(get_links())
